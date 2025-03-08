c77c0ad904d037a7c54577b4e9a27d49ff8a18ea
Oscar Somba <maingioscar2@gmail.com>
2025-03-09 00:55:53 +0300

    Used the class property to store a list of puppies with common but different characteristics to avoid dry programming

--- wednesday.js
+++ wednesday.js
@@ -1,13 +1,54 @@
-// console.log(3 * (10 - 4));
+// DCCV; // console.log(3 * (10 - 4));
 let studentName = "Stella";
 // console.log(studentName);
 const radius = 2;
+// const radius =3;
 const pi = 3.14;
 const areaOfCircle = pi * (radius * 2);
 `console.log(areaOfCircle);`;
-// // const piii=3.14;
-// let email = "maingioscar2@gmail.com";
-// let password = "k123";
-// console.log(password);
+const piii = 3.14;
+let email = "maingioscar2@gmail.com";
+let password = "k123";
+console.log(password);
+console.log(email);
+// fxxxxxxxxxxxxxxxxxxxxxxx;
 console.log(typeof pi);
+console.log(typeof piii);
 console.log(typeof studentName);
+console.log(areaOfCircle);
+console.log(typeof areaOfCircle);
+const pet = "fifi";
+// const pet = "fifi";
+console.log(email);
+const cat = "fifi";
+console.log(cat.length);
+if (name.length < 6) {
+  console.log("Log in");
+}
+// const pets = ["fifi", "fifi 1", "fifi 2", "fifi 3"];
+// console.log(pets);
+const pets = {
+  name: "fifi",
+  age: 17,
+  owner: "Karonji",
+};
+
+class Puppies {
+  constructor(name, age, owner) {
+    this.name = name;
+    this.age = age;
+    this.owner = owner;
+  }
+}
+
+const puppy1 = new Puppies("Fifi", 17, "Stella");
+const puppy2 = new Puppies("Chiwi", 18, "Oscar");
+const puppy3 = new Puppies("Hafsa", 23, "Mike");
+const puppy4 = new Puppies("Kyme", 13, "Martha");
+const puppy5 = new Puppies("Hafsa", 24, "Egal");
+console.log(puppy1);
+console.log(puppy2);
+console.log(puppy3);
+console.log(puppy4);
+console.log(puppy5);
+console.log(typeof Puppies);
